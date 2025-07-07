d56fb9dc3f2a0e6fea0e823a26ed32c3471950fd
csheehan18 <csheehan118@gmail.com>
2025-07-07 00:38:38 -0400

    SetupJail Added

--- src/main/java/com/corn/plugin/JailPlugin.java
+++ src/main/java/com/corn/plugin/JailPlugin.java
@@ -7,29 +7,14 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import io.papermc.lib.PaperLib;
 
-/**
- * Main plugin class for JailPlugin.
- */
 public class JailPlugin extends JavaPlugin {
 
-  /**
-   * Called when the plugin is enabled.
-   */
   @Override
   public void onEnable() {
     PaperLib.suggestPaper(this);
     saveDefaultConfig();
   }
 
-  /**
-   * Handles commands registered in plugin.yml.
-   *
-   * @param sender  the command sender
-   * @param command the command object
-   * @param label   the command alias used
-   * @param args    command arguments
-   * @return true if the command was handled, false otherwise
-   */
   @Override
   public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
     if ("hello".equalsIgnoreCase(command.getName())) {
@@ -40,6 +25,27 @@ public class JailPlugin extends JavaPlugin {
       }
       return true;
     }
+    
+    if ("setupJail".equalsIgnoreCase(command.getName())) {
+      if (!(sender instanceof Player)) {
+        sender.sendMessage("This command can only be run by a player.");
+        return true;
+      }
+      Player player = (Player) sender;
+      // Get player location
+      var loc = player.getLocation();
+
+      // Save coordinates to config
+      getConfig().set("jail.world", loc.getWorld().getName());
+      getConfig().set("jail.x", loc.getX());
+      getConfig().set("jail.y", loc.getY());
+      getConfig().set("jail.z", loc.getZ());
+      saveConfig();
+
+      player.sendMessage("Jail location saved at your current position!");
+      return true;
+    }
+
     return false;
   }
 }
