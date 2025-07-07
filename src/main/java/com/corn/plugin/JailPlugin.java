4ab194253551849125b1efd96be413a0a4fba91a
csheehan18 <csheehan118@gmail.com>
2025-07-07 00:24:04 -0400

    Update JailPlugin.java

--- src/main/java/com/corn/plugin/JailPlugin.java
+++ src/main/java/com/corn/plugin/JailPlugin.java
@@ -1,19 +1,45 @@
 package com.corn.plugin;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import io.papermc.lib.PaperLib;
 
-
 /**
- * Created by Corn
+ * Main plugin class for JailPlugin.
  */
 public class JailPlugin extends JavaPlugin {
 
+  /**
+   * Called when the plugin is enabled.
+   */
   @Override
   public void onEnable() {
     PaperLib.suggestPaper(this);
-
     saveDefaultConfig();
+  }
+
+  /**
+   * Handles commands registered in plugin.yml.
+   *
+   * @param sender  the command sender
+   * @param command the command object
+   * @param label   the command alias used
+   * @param args    command arguments
+   * @return true if the command was handled, false otherwise
+   */
+  @Override
+  public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    if ("hello".equalsIgnoreCase(command.getName())) {
+      if (sender instanceof Player) {
+        sender.sendMessage("Hello!");
+      } else {
+        sender.sendMessage("This command can only be run by a player.");
+      }
+      return true;
+    }
+    return false;
   }
 }
