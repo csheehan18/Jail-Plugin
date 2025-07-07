3d65015f008885e4231a7716456021f11f1638df
csheehan18 <csheehan118@gmail.com>
2025-07-07 00:06:42 -0400

    Update JailPlugin.java

--- src/main/java/com/corn/plugin/JailPlugin.java
+++ src/main/java/com/corn/plugin/JailPlugin.java
@@ -1,15 +1,31 @@
 package com.corn.plugin;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import io.papermc.lib.PaperLib;
 
 public class JailPlugin extends JavaPlugin {
 
-  @Override
-  public void onEnable() {
-    PaperLib.suggestPaper(this);
+    @Override
+    public void onEnable() {
+        PaperLib.suggestPaper(this);
+        saveDefaultConfig();
+    }
 
-    saveDefaultConfig();
-  }
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("hello")) {
+            if (sender instanceof Player) {
+                sender.sendMessage("Hello!");
+                return true;
+            } else {
+                sender.sendMessage("This command can only be run by a player.");
+                return true;
+            }
+        }
+        return false; // command not recognized by this plugin
+    }
 }
